fb864deb3dcede5b9908b3463eed1412ba31c6cd
Cody Burrows <dcodyburrows@gmail.com>
2020-04-03 09:59:11 -0400

    Added stub for AC97 header
    - The include guard works flawlessly

---
+++ ac97.h
@@ -0,0 +1,15 @@
+/*
+** File:	ac97.h
+**
+** Author:	Cody Burrows (cxb2114@rit.edu)
+**
+** Contributor:
+**
+** Description:	Contains constants and function declarations for the baseline
+** ac97 driver.
+*/
+
+#ifndef _AC97_H_
+#define _AC97_H_
+
+#endif
