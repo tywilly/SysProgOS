b38c1197fd025a382ade7c945ea82e79e6c91aac
Yann <yann.moullec@insa-rennes.fr>
2020-04-28 16:08:02 +0200

    Comments, comments everywhere

--- pci.c
+++ pci.c
@@ -1,17 +1,23 @@
+/*
+** File:    pci.c
+**
+** Author:  Yann MOULLEC
+**
+** Contributor:
+**
+** Description: Implementation of the pci module
+*/
+
 #define __SP_KERNEL__
 
 #include "common.h"
-
 #include "pci.h"
 
-// Header commons offset
-// register 	offset 	bits 31-24 	bits 23-16 	bits 15-8 	bits 7-0
-// 00 	00 	Device ID 	Vendor ID
-// 01 	04 	Status 	Command
-// 02 	08 	Class code 	Subclass 	Prog IF 	Revision ID
-// 03 	0C 	BIST 	Header type 	Latency Timer 	Cache Line Size
+/*
+** PRIVATE DEFINITIONS
+*/
 
-// PCI Device Structure
+// PCI Device Structure offsets
 // common to all header types
 #define PCI_VENDOR_ID       0
 #define PCI_DEVICE_ID       0x2
@@ -25,27 +31,37 @@
 #define PCI_LATENCY_TIMER   0xD
 #define PCI_HEADER_TYPE     0xE
 #define PCI_BIST            0xF
-
+// other useful offsets
 #define PCI_BAR0            0x10
 #define PCI_SEC_BUS         0x19
 
-/**
- * Private variables
+// (Arbitrary) value for maximum number of connected devices
+// to the PCI
+#define PCI_MAX_DEVICES 15
+
+/*
+** PRIVATE GLOBAL VARIABLES
 */
 
-static PCIDevice _pci_dev_list[MAX_PCI_DEVICES];
+// The PCI module stores all static device information in an array
+static PCIDevice _pci_dev_list[PCI_MAX_DEVICES];
 static int _pci_num_dev = 0;
 
-/**
- * Resolving cyclic function dependencies
+/*
+** PRIVATE FUNCTIONS
 */
 
+// forward declaration
 static void _pci_check_bus( uint8 );
 
-/**
- * Private functions
-*/
-
+//
+// _pci_check_function() - check the function for a connected device
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//
 static void _pci_check_function( uint8 bus, uint8 device, uint8 function ) {
     uint32 bar0;
     uint16 vendorID;
@@ -56,6 +72,7 @@ static void _pci_check_function( uint8 bus, uint8 device, uint8 function ) {
     uint8 secondaryBus;
     uint8 headerType;
 
+    // read all function information
     class = _pci_cfg_read_b( bus, device, function, PCI_CLASS );
     subClass = _pci_cfg_read_b( bus, device, function, PCI_SUBCLASS );
     progIF = _pci_cfg_read_b( bus, device, function, PCI_PROG_IF );
@@ -64,11 +81,14 @@ static void _pci_check_function( uint8 bus, uint8 device, uint8 function ) {
     deviceID = _pci_cfg_read_w( bus, device, function, PCI_DEVICE_ID );
     bar0 = _pci_cfg_read_l( bus, device, function, PCI_BAR0 );
     
+    // check if the device is a secondary bus
+    // if so, check the bus for connected devices
     if(( class == 0x06 ) && ( subClass == 0x04 ) && ( headerType == 0x01 )) {
         secondaryBus = _pci_cfg_read_b( bus, device, function, PCI_SEC_BUS );
         _pci_check_bus( secondaryBus );
     }
 
+    // store device information
     PCIDevice *newDevice = &_pci_dev_list[_pci_num_dev];
     newDevice->bus = bus;
     newDevice->device = device;
@@ -79,6 +99,7 @@ static void _pci_check_function( uint8 bus, uint8 device, uint8 function ) {
     newDevice->vendorID = vendorID;
     newDevice->deviceID = deviceID;
 
+    // read base address 0 properly
     if( bar0 & 1 ) {                        // I/O Space BAR layout
         newDevice->bar0 = bar0 & 0xFFFFFFFC;
     } else {                                // Memory Space BAR Layout
@@ -93,6 +114,13 @@ static void _pci_check_function( uint8 bus, uint8 device, uint8 function ) {
     _pci_num_dev ++;
 }
 
+//
+// _pci_check_device() - check the device number for connected devices
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//
 static void _pci_check_device( uint8 bus, uint8 device ) {
     uint8 function = 0;
 
@@ -102,6 +130,8 @@ static void _pci_check_device( uint8 bus, uint8 device ) {
     
     _pci_check_function( bus, device, function );
 
+    // check if multifunction device
+    // if so, check all functions
     uint8 headerType = _pci_cfg_read_b( bus, device, function, PCI_HEADER_TYPE );
     if(( headerType & 0x80 ) != 0) {
         for( function = 1; function < 8; function++ ) {
@@ -112,18 +142,31 @@ static void _pci_check_device( uint8 bus, uint8 device ) {
     }
 }
 
+//
+// _pci_check_bus() - check the bus for connected devices
+//
+// Parameters:
+//    bus           bus number
+//
 static void _pci_check_bus( uint8 bus ) {
     uint8 device;
 
+    // check all devices on the bus
     for( device = 0; device < 32; device++ ) {
         _pci_check_device( bus, device );
     }
 }
 
+//
+// _pci_check_buses() - check all buses for connected devices
+//
 static void _pci_check_buses() {
     uint8 function;
     uint8 bus;
 
+    // check if multifunction device
+    // if so, check all functions as buses
+    // if not check bus 0
     uint8 headerType = _pci_cfg_read_b( 0, 0, 0, PCI_HEADER_TYPE );
     if(( headerType & 0x80 ) == 0) {
         _pci_check_bus( 0 );
@@ -136,10 +179,54 @@ static void _pci_check_buses() {
     }
 }
 
-/**
- * Public functions
+/*
+** PUBLIC FUNCTIONS
 */
 
+//
+// _pci_init() - checks all buses for connected devices
+//
+void _pci_init() {
+    _pci_check_buses();
+    __cio_puts( " PCI" );
+}
+
+//
+// _pci_dev_class() - get the first device descriptor matching the given class
+//
+// Parameters:
+//    class         class code
+//    subclass      subclass code
+//    progIF        programming interface code
+//
+// Returns:
+//    a pointer to the device descriptor
+//
+PCIDevice *_pci_dev_class( uint8 class, uint8 subClass, uint8 progIF ) {
+    uint8 i;
+
+    for( i = 0; i < _pci_num_dev; i++ ) {
+        PCIDevice *dev = &_pci_dev_list[i];
+        if( dev->class == class && dev->subClass == subClass && dev->progIF == progIF ) {
+            return( dev );
+        }
+    }
+
+    return( NULL );
+}
+
+//
+// _pci_cfg_read_l() - read a long word from the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//
+// Returns:
+//    the long word read
+//
 uint32 _pci_cfg_read_l( uint8 bus, uint8 device, uint8 function, uint8 offset ) {
     uint32 address;
     uint32 lbus  = (uint32)bus;
@@ -155,23 +242,52 @@ uint32 _pci_cfg_read_l( uint8 bus, uint8 device, uint8 function, uint8 offset ) 
     return( __inl( 0xCFC ) );
 }
 
+//
+// _pci_cfg_read_w() - read a word from the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//
+// Returns:
+//    the word read
+//
 uint16 _pci_cfg_read_w( uint8 bus, uint8 device, uint8 function, uint8 offset ) {
     uint32 tmp = _pci_cfg_read_l( bus, device, function, offset );
     // (offset & 2) * 8) = 0 will choose the first word of the 32 bits register
     return( (uint16)((tmp >> ((offset & 2) * 8)) & 0xFFFF) );
 }
 
+//
+// _pci_cfg_read_b() - read a byte from the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//
+// Returns:
+//    the byte read
+//
 uint8 _pci_cfg_read_b( uint8 bus, uint8 device, uint8 function, uint8 offset ) {
     uint16 tmp = _pci_cfg_read_w( bus, device, function, offset );
     // use offset to pick the right byte in tmp
     return(( tmp >> ((offset & 1) * 8)) & 0xFF );
 }
 
-void _pci_init() {
-    _pci_check_buses();
-    __cio_puts( " PCI" );
-}
-
+//
+// _pci_cfg_write_l() - write a long word in the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//    value         value to be written
+//
 void _pci_cfg_write_l( uint8 bus, uint8 device, uint8 function, uint8 offset, uint32 value ) {
     uint32 address;
     uint32 lbus  = (uint32)bus;
@@ -185,6 +301,16 @@ void _pci_cfg_write_l( uint8 bus, uint8 device, uint8 function, uint8 offset, ui
 	__outl( value, 0xCFC );
 }
 
+//
+// _pci_cfg_write_w() - write a word in the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//    value         value to be written
+//
 void _pci_cfg_write_w( uint8 bus, uint8 device, uint8 function, uint8 offset, uint16 value ) {
     uint32 newValue;
     uint16 content;
@@ -195,6 +321,16 @@ void _pci_cfg_write_w( uint8 bus, uint8 device, uint8 function, uint8 offset, ui
 	_pci_cfg_write_l( bus, device, function, offset, newValue);
 }
 
+//
+// _pci_cfg_write_b() - write a byte in the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//    value         value to be written
+//
 void _pci_cfg_write_b( uint8 bus, uint8 device, uint8 function, uint8 offset, uint8 value ) {
     uint16 newValue;
     uint8 content;
@@ -205,39 +341,67 @@ void _pci_cfg_write_b( uint8 bus, uint8 device, uint8 function, uint8 offset, ui
     _pci_cfg_write_w( bus, device, function, offset, newValue);
 }
 
+//
+// _pci_set_command() - set the device command register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    value         value to be written
+//
 void _pci_set_command( uint8 bus, uint8 device, uint8 function, uint16 value ) {
     _pci_cfg_write_w( bus, device, function, PCI_COMMAND, value );
 }
 
-
+//
+// _pci_set_interrupt() - set the device interrupt line and pin registers
+//
+// Parameters:
+//    bus               bus number
+//    device            device number
+//    function          function number
+//    interruptPin      value to be written in interruptPin
+//    interruptLine     value to be written in interruptLine
+//
 void _pci_set_interrupt( uint8 bus, uint8 device, uint8 function, uint8 interruptPin, uint8 interruptLine ) {
     uint16 interrupt = (uint16)(interruptPin << 8) & interruptLine;
     _pci_cfg_write_w( bus, device, function, 0x3C, interrupt );
 }
 
-
+//
+// _pci_get_interrupt_line() - get the device interrupt line register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//
+// Returns:
+//    the interrupt line register
+//
 uint8 _pci_get_interrupt_line( uint8 bus, uint8 device, uint8 function ) {
     return _pci_cfg_read_b( bus, device, function, 0x3C );
 }
 
-
+//
+// _pci_get_status() - get the device status register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//
+// Returns:
+//    the status register
+//
 uint16 _pci_get_status( uint8 bus, uint8 device, uint8 function ) {
     return( _pci_cfg_read_w( bus, device, function, PCI_STATUS ));
 }
 
-PCIDevice *_pci_dev_class( uint8 class, uint8 subClass, uint8 progIF ) {
-    uint8 i;
-
-    for( i = 0; i < _pci_num_dev; i++ ) {
-        PCIDevice *dev = &_pci_dev_list[i];
-        if( dev->class == class && dev->subClass == subClass && dev->progIF == progIF ) {
-            return( dev );
-        }
-    }
-
-    return( NULL );
-}
-
+//
+// _pci_dump_all() - dump all device information to the console
+//
 void _pci_dump_all() {
     for(int i = 0; i<_pci_num_dev; i++) {
         PCIDevice *dev = &_pci_dev_list[i];
@@ -246,6 +410,15 @@ void _pci_dump_all() {
     }
 }
 
+//
+// _pci_dump_header() - dump device header information to the console
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    nlines        number of header lines to dump
+//
 void _pci_dump_header( uint8 bus, uint8 device, uint8 function, uint8 nlines ) {
     uint8 i;
     __cio_puts( "\nHEADER CONTENTS:\n" );
--- pci.h
+++ pci.h
@@ -1,7 +1,27 @@
+/*
+** File:    pci.h
+**
+** Author:  Yann MOULLEC
+**
+** Contributor:
+**
+** Description: PCI module declarations 
+*/
+
 #ifndef _PCI_H_
 #define _PCI_H_
 
-#define MAX_PCI_DEVICES 15
+/*
+** General (C and/or assembly) definitions
+*/
+
+/*
+** Start of C-only definitions
+*/
+
+/*
+** Types
+*/
 
 typedef struct pci_device_s {
     uint8 bus;
@@ -15,25 +35,185 @@ typedef struct pci_device_s {
     uint32 bar0;
 } PCIDevice;
 
+/*
+** Prototypes
+*/
+
+//
+// _pci_init() - checks all buses for connected devices
+//
 void _pci_init( void );
 
-uint32 _pci_cfg_read_l( uint8 bus, uint8 device, uint8 function, uint8 offset );
-uint16 _pci_cfg_read_w( uint8 bus, uint8 device, uint8 function, uint8 offset );
-uint8 _pci_cfg_read_b( uint8 bus, uint8 device, uint8 function, uint8 offset );
-
-void _pci_cfg_write_l( uint8 bus, uint8 device, uint8 function, uint8 offset, uint32 value );
-void _pci_cfg_write_w( uint8 bus, uint8 device, uint8 function, uint8 offset, uint16 value );
-void _pci_cfg_write_b( uint8 bus, uint8 device, uint8 function, uint8 offset, uint8 value );
-
-void _pci_set_command( uint8 bus, uint8 device, uint8 function, uint16 value );
-uint16 _pci_get_status( uint8 bus, uint8 device, uint8 function );
-
-void _pci_set_interrupt( uint8 bus, uint8 device, uint8 function, uint8 interruptPin, uint8 interruptLine );
-uint8 _pci_get_interrupt_line( uint8 bus, uint8 device, uint8 function );
-
+//
+// _pci_dev_class() - get the first device descriptor matching the given class
+//
+// Parameters:
+//    class         class code
+//    subclass      subclass code
+//    progIF        programming interface code
+//
+// Returns:
+//    a pointer to the device descriptor
+//
 PCIDevice *_pci_dev_class( uint8 class, uint8 subClass, uint8 progIF );
 
+//
+// General read functions
+//
+
+//
+// _pci_cfg_read_l() - read a long word from the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//
+// Returns:
+//    the long word read
+//
+uint32 _pci_cfg_read_l( uint8 bus, uint8 device, uint8 function, uint8 offset );
+
+//
+// _pci_cfg_read_w() - read a word from the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//
+// Returns:
+//    the word read
+//
+uint16 _pci_cfg_read_w( uint8 bus, uint8 device, uint8 function, uint8 offset );
+
+//
+// _pci_cfg_read_b() - read a byte from the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//
+// Returns:
+//    the byte read
+//
+uint8 _pci_cfg_read_b( uint8 bus, uint8 device, uint8 function, uint8 offset );
+
+//
+// General write functions
+//
+
+//
+// _pci_cfg_write_l() - write a long word in the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//    value         value to be written
+//
+void _pci_cfg_write_l( uint8 bus, uint8 device, uint8 function, uint8 offset, uint32 value );
+
+//
+// _pci_cfg_write_w() - write a word in the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//    value         value to be written
+//
+void _pci_cfg_write_w( uint8 bus, uint8 device, uint8 function, uint8 offset, uint16 value );
+
+//
+// _pci_cfg_write_b() - write a byte in the device configuration register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    offset        configuration register offset
+//    value         value to be written
+//
+void _pci_cfg_write_b( uint8 bus, uint8 device, uint8 function, uint8 offset, uint8 value );
+
+//
+// Register specific functions
+//
+
+//
+// _pci_set_command() - set the device command register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    value         value to be written
+//
+void _pci_set_command( uint8 bus, uint8 device, uint8 function, uint16 value );
+
+//
+// _pci_set_interrupt() - set the device interrupt line and pin registers
+//
+// Parameters:
+//    bus               bus number
+//    device            device number
+//    function          function number
+//    interruptPin      value to be written in interruptPin
+//    interruptLine     value to be written in interruptLine
+//
+uint16 _pci_get_status( uint8 bus, uint8 device, uint8 function );
+
+//
+// _pci_get_interrupt_line() - get the device interrupt line register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//
+// Returns:
+//    the interrupt line register
+//
+void _pci_set_interrupt( uint8 bus, uint8 device, uint8 function, uint8 interruptPin, uint8 interruptLine );
+
+//
+// _pci_get_status() - get the device status register
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//
+// Returns:
+//    the status register
+//
+uint8 _pci_get_interrupt_line( uint8 bus, uint8 device, uint8 function );
+
+//
+// Debugging function
+//
+
+//
+// _pci_dump_all() - dump all device information to the console
+//
 void _pci_dump_all( void );
+
+//
+// _pci_dump_header() - dump device header information to the console
+//
+// Parameters:
+//    bus           bus number
+//    device        device number
+//    function      function number
+//    nlines        number of header lines to dump
+//
 void _pci_dump_header( uint8 bus, uint8 device, uint8 function, uint8 nlines );
 
 #endif
