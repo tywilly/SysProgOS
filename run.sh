3bcfe1bf773451ef197539d8cba2c2337eb43648
Cody Burrows <dcodyburrows@gmail.com>
2020-05-05 09:01:49 -0400

    Improved run.sh
    At Yann's suggestion, I added arguments to add the USB options, and the CDROM
    options.
    
    I also fixed the line lengths so they're not horribly long.

--- run.sh
+++ run.sh
@@ -8,14 +8,34 @@
 TTY=$(tty)
 echo "Running from $TTY"
 
-BASE="qemu-system-i386 -drive file=usb.image,index=0,media=disk,format=raw -serial mon:"$TTY" "
+BASE="qemu-system-i386 -drive file=usb.image,index=0,media=disk,format=raw "
+BASE+="-serial mon:"$TTY" "
 SOUND_PREFIX="-soundhw "
 AC97="ac97"
 SB="es1370"
 CDROM="-cdrom /dev/cdrom "
-USB="-usb -device usb-ehci,id=usb-ehci -device usb-storage,bus=usb-ehci.0,drive=usbstick "
+USB="-drive if=none,id=usbstick,file=flashdrive.img,format=raw "
+USB+="-usb -device usb-ehci,id=usb-ehci "
+USB+="-device usb-storage,bus=usb-ehci.0,drive=usbstick "
+USAGE="Usage: ./run.sh [usb, cdrom]"
+COMMAND="$BASE""$SOUND_PREFIX""$AC97"",""$SB"" "
 
-# eval "$BASE""$SOUND_PREFIX""$AC97"",""$SB"" ""$CDROM""$USB"
-eval "$BASE""$SOUND_PREFIX""$AC97"",""$SB"" ""$CDROM"
+# a fine variable for perusing the command line arguments
+for foo in "$@"
+do
+    if [ "$foo" = "usb" ]; then
+        COMMAND+="$USB"
+    elif [ "$foo" = "cdrom" ]; then
+        COMMAND+="$CDROM"
+    else
+        echo "$USAGE" && exit
+    fi
+done
 
-reset
+eval "$COMMAND"
+
+# QEMU leaves my terminal windows unusable for some reason. If there wasn't
+# an error status, reset it so it's usable again.
+if [ "$?" = 0 ]; then
+    reset
+fi
