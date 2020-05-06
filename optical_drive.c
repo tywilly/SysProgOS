01284015a620fd51b947a54cb41bbef22b937042
Michael Hopkins <mbh4480@rit.edu>
2020-05-05 23:42:40 -0400

    working read fixing endianess error for capacity

--- optical_drive.c
+++ optical_drive.c
@@ -8,9 +8,16 @@
 #include "common.h"
 #include "process.h"
 #include "pci.h"
+struct prdt prdt_t[1];
 
-volatile uint32 ide_secondary_interrupt = 0;
+//setup prdt for DMA not used and not finished
+//*prdt[]  _prdt_setup(void){
+//	prdt_t[0].mba = 1;
+//	_pci_set_command(pci_dev->bus, pci_dev->device, pci_dev->function, 0x5);
 
+//}
+
+//handles interrupts for pio mode 
 void _atapi_isr_secondary(int vector, int code){
      //interrupt handles rest of pio mode get size from lba registers
      int size = ( ((unsigned short)(__inb(ATA_DATA(ATA_BUS_SECONDARY)) << 8)) | 
@@ -18,11 +25,15 @@ void _atapi_isr_secondary(int vector, int code){
      for(int i=0; i<size; i++){ 
         __cio_printf("%x",__inw(ATA_DATA(ATA_BUS_SECONDARY)));
      }
-     //acknowledge interrupt so it can happen again
+     __cio_printf("\n");
+     //acknowledge interrupt so it can happen again for some reason it is not firing again
      __outb(PIC_MASTER_CMD_PORT,PIC_EOI);
 }
 
+//read capacity scsi command and print out information from it
 uint32 _atapi_capacity_process(uint32 bus) {
+    
+    __delay(100);
     uint32 status;
     __outb(ATA_FEATURES(bus), 0); // PIO mode
     //for read capacity low and mid registers are set to 0x08
@@ -35,8 +46,6 @@ uint32 _atapi_capacity_process(uint32 bus) {
     __cio_printf("status_reg: %x\n", status);
     if (status & ATA_STATUS_ERROR) {
 	__cio_printf("error sending packet command");
-        WARNING("error sending packet command\n");
-    	return 0;
     }
     __outw(ATA_DATA(bus), ATAPI_CAPACITY_READ);
     __outw(ATA_DATA(bus), 0);
@@ -46,21 +55,34 @@ uint32 _atapi_capacity_process(uint32 bus) {
     __outw(ATA_DATA(bus), 0);
     while ((status = __inb(ATA_COMMAND(bus))) & ATA_STATUS_BUSY)
         __asm__ __volatile__ ("pause");
-    __cio_printf("status_reg: %x\n", status);
+    __cio_printf("\nstatus_reg: %x\n", status);
     if (status & ATA_STATUS_ERROR) {
 	__cio_printf("error sending packet command");
         WARNING("error sending scsi command\n");
         return 0;
     }
-    uint32 last_lba = __inw(ATA_DATA(bus));
-    last_lba = last_lba | ((uint32) __inw(ATA_DATA(bus)) << 16);
-    uint32 block_size = __inw(ATA_DATA(bus));
-    block_size = block_size | ((uint32) __inw(ATA_DATA(bus)) << 16);
-    return block_size;
+   // uint32 lba = __inw(ATA_DATA(bus));
+   // lba = lba | ((uint32) __inw(ATA_DATA(bus)) << 16);
+   // uint32 block = __inw(ATA_DATA(bus));
+    //lock = block | ((uint32) __inw(ATA_DATA(bus)) << 16);
+   // __cio_printf("\nLAST LBA: %x\n", lba);
+   uint32 data1 = __inw(ATA_DATA(bus));
+   uint32 data2 = __inw(ATA_DATA(bus));
+   uint32 data3 = __inw(ATA_DATA(bus));
+   uint32 data4 = __inw(ATA_DATA(bus));
+   return 1;
 }
-int _atapi_dma_read_process(uint32 bus, uint32 drive) {
-    //setup prdt
+
+//DMA version of read with DMA specific changes is not 
+//currently not working failing to send scsi command I am not using at the moment
+//despite the plan to due to confusion over setting data bits for DMA mode
+int _atapi_dma_read_process(uint32 bus, uint32 sector) {
+    //setup prdt not finished
+  //  *this_prdt = prdt_setup();
+  //  would output long prdt address to pci bus master 0xC to 0xF
     uint32 status;
+    //send read command to bus master
+    __outb(0xC8,0x8);
     //enable DMA
     __outb(ATA_FEATURES(bus), 1);
    // set MID and LOW to zero because of DMA usage
@@ -82,20 +104,23 @@ int _atapi_dma_read_process(uint32 bus, uint32 drive) {
      __outb(ATAPI_INTERRUPT_REG,ATA_DCR_HIGH_ORDER_BYTE);
     __outw(ATA_DATA(bus), ATAPI_READ_COMMAND);
     // slave bit portion.
-    __outw(ATA_DATA(bus), (uint16)(drive >> 16));
-    __outw(ATA_DATA(bus), (uint16)drive);
     __outw(ATA_DATA(bus), 0);
-    __outw(ATA_DATA(bus), 1);
-    __outw(ATA_DATA(bus), 0);
+    __outw(ATA_DATA(bus), sector >> 24 & 0x0f);
+    __outw(ATA_DATA(bus), sector >> 16 & 0xff);
+    //ADDR low and high values (as seen in pio implementation) go here in DMA mode from what I can tell
+    __outw(ATA_DATA(bus), (ATAPI_SECTOR_SIZE >> 8));
+    __outw(ATA_DATA(bus), (ATAPI_SECTOR_SIZE & 0xff));
     if (status & ATA_STATUS_ERROR) {
         WARNING("error sending scsi command\n");
         return 0;
     }
     //success
+    //would need a case in interrupt handler to handle the buffer
     return 1;
 }
 
-int _atapi_read_process(uint32 bus, uint32 drive) {
+//standard PIO read that actually works mostly
+int _atapi_read_process(uint32 bus, uint32 sector) {
     uint32 status;
   //  __outb(ATAPI_INTERRUPT_REG,ATA_DCR_HIGH_ORDER_BYTE);
    // uint16 size=0;
@@ -114,17 +139,19 @@ int _atapi_read_process(uint32 bus, uint32 drive) {
 	__cio_printf("error sending packet command");
         WARNING("error sending packet command\n");
     	return 0;
-    }
+    } 
+    //enable interrupts here instead to avoid inital interrupt and only grab the read interrupts
     //So this looks weird but atapi commands are sent in several outw portions to different ports to emulate
-    //SCSI commands 
+    //SCSI commands
      __outb(ATAPI_INTERRUPT_REG,ATA_DCR_HIGH_ORDER_BYTE);
     __outw(ATA_DATA(bus), ATAPI_READ_COMMAND);
-    // slave bit portion.
-    __outw(ATA_DATA(bus), (uint16)(drive >> 16));
-    __outw(ATA_DATA(bus), (uint16)drive);
+    // sector portion
+    __outw(ATA_DATA(bus), (uint16)(sector >> 16));
+    __outw(ATA_DATA(bus), (uint16)sector);
     __outw(ATA_DATA(bus), 0);
     __outw(ATA_DATA(bus), 1);
     __outw(ATA_DATA(bus), 0);
+    //check for immediate errors 
     if (status & ATA_STATUS_ERROR) {
 	__cio_printf("error sending packet command");
         WARNING("error sending scsi command\n");
@@ -134,28 +161,37 @@ int _atapi_read_process(uint32 bus, uint32 drive) {
     return 1;
 }
 
+//initizilation function that installs the secondary ata irq
+//selects the default drive then delays and checks status register to see if it is ready
 void _atapi_init(void) {
     __install_isr(ATA_IRQ_SECONDARY + PIC_EOI, _atapi_isr_secondary);
     //Slightly confusing start up 
    // enables drive interrupts which I do not want to do until a read is executed
+   // Orginally enabled interrupts here but I can just poll on BSY or DRQ  for the packet command
    // __outb(ATAPI_INTERRUPT_REG,ATA_DCR_HIGH_ORDER_BYTE);
     //sets drive select to master 0x176 IS THE register for setting which drive to use
     __outb(DRIVE_SELECT,SELECT_MASTER);
+    //delay at startup for drive selection since it is not instant and needs some time before
+    //it can recieve packet commands. That long init at the start is caused by this delay. 
     __delay(100);
     if(__inb(CD_READY) & 0x40 ){
-	__cio_puts(" ATAPI");
+	__cio_puts(" ATAPI ");
     }
     return;
 }
 
+//basic test function that calls the actual read process (PIO mode) It uses ATA_BUS_SECONDARY since 
+//I had trouble detecting the default drive for qemu (which ATA_BUS_SECONDARY is) 
+//it only does a read of 1 sector of the cd
 void _atapi_read(void) {
-   	if(_atapi_read_process(ATA_BUS_SECONDARY,SECONDARY_MASTER)) {
-     	    __cio_puts( "Read Attempt:/n" );
+   	if(_atapi_read_process(ATA_BUS_SECONDARY,1)) {
+     	    __cio_puts( "Read Attempt:" );
    	}
 	return;
 } 
 
+//basic test function that calls the actual read capacity
 void _atapi_capacity(void) {
-	__cio_printf("%x",_atapi_capacity_process(ATA_BUS_SECONDARY));
+	__cio_printf("Block Size:%x\n",_atapi_capacity_process(ATA_BUS_SECONDARY));
 	return;
 }
--- optical_drive.h
+++ optical_drive.h
@@ -1,31 +1,50 @@
 #ifndef OPTICAL_DRIVE
-#define OPTCIAL_DRIVE
+#define OPTICAL_DRIVE
 
-/* The default and seemingly universal sector size for CD-ROMs. */
+//prdt struct for DMA not used as did not get DMA working
+struct prdt {
+	uint32 offset;
+	uint16 bytes; 
+	uint16 mba;
+};
+
+/* The default  sector size for CD-ROMs. */
 #define ATAPI_SECTOR_SIZE 2048
  
-/* The default ISA IRQ numbers of the ATA controllers. */
+/* The default ISA IRQ number of the ATA controllers. */
 #define ATA_IRQ_SECONDARY 0x0F
 
+//Primary and Secondary buses qemu defaults on secondary form what I have seen
 #define ATA_BUS_PRIMARY 0x1F0
 #define ATA_BUS_SECONDARY 0x170
+//drive select register for selecting desired drive
 #define DRIVE_SELECT 0x176
+//cd ready register to check if BSY
 #define CD_READY 0x177
+//interrupt register for enabling / disabling ATAPI interrupts
 #define ATAPI_INTERRUPT_REG 0x376
+//For drive select
 #define SELECT_MASTER 0x00
 
 #define ATA_MASTER 0xA0
 #define ATA_SLAVE 0xB0
 
+//Packet command the most used command for 
+//ATAPI the packet in packet interface
 #define ATA_PACKET_COMMAND 0xa0
 
+//Drives
 #define PRIMARY_MASTER 0x01f000a0
 #define PRIMARY_SLAVE 0x01f000b0
 #define SECONDARY_MASTER 0x017000a0
 #define SECONDARY_SLAVE 0x017000b0
 
+#define ATAPI_CAPACITY_READ 0X25
 #define ATA_PIO_MODE 0
-#define ATAPI_READ_COMMAND 0xa0
+#define ATA_DMA_MODE 1
+#define ATAPI_READ_COMMAND 0xa8
+
+//ATAPI general register structure based off bus given taken from OSdevwiki
 #define ATA_DATA(x)           (x)
 #define ATA_FEATURES(x)      (x+1)
 #define ATA_SECTOR_COUNT(x)   (x+2)
@@ -36,12 +55,15 @@
 #define ATA_COMMAND(x)        (x+7)
 #define ATA_DCR(x)        (x+0x206)
 
+//Delay on the select drive from OSdevwiki not used at the moment
 #define ATA_SELECT_DELAY(bus) \
   {__inb(ATA_DCR(bus));__inb(ATA_DCR(bus));__inb(ATA_DCR(bus));__inb(ATA_DCR(bus));}
 
+//identify commands not used at the moment
 #define ATAPI_COMMAND_IDENTIFY 0xa1
 #define ATA_COMMAND_IDENTIFY 0xec
 
+//Status Problems
 #define ATA_STATUS_BUSY 0x80
 #define ATA_STATUS_READY 0x40
 #define ATA_STATUS_WRITE_FAULT 0x20
@@ -51,6 +73,8 @@
 #define ATA_STATUS_INDEX 0x2
 #define ATA_STATUS_ERROR 0x1
 
+//Types of errors I would have liked to implement,but didn't get the chance
+//to in a more meaningful form
 #define ATA_ERROR_BAD_SECTOR 0x80
 #define ATA_ERROR_UNCORRECTABLE_DATA 0x40
 #define ATA_ERROR_NO_MEDIA 0x20
@@ -60,12 +84,14 @@
 #define ATA_ERROR_TRACK0_NOT_FOUND 0x02
 #define ATA_ERROR_ERROR 0x01
 
+//DCR 
 #define ATA_DCR_STOP 0x01
 #define ATA_DCR_SOFTWARE_RESET 0x04
 #define ATA_DCR_HIGH_ORDER_BYTE 0x80
 
 #define ATAPI_CAPACITY_READ 0X25
 #define ATAPI_PACKET 0xa0
+
 void _atapi_capacity(void);
 void _atapi_isr_secondary(int vector,int code);
 void _atapi_init(void);
